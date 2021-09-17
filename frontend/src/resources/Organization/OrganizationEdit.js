2cd14eb2769bbc72da70f7758efd6455f998cc82
simon <simon.louvet.zen@gmail.com>
2021-09-17 18:50:34 +0200

    add proximity

--- frontend/src/resources/Organization/OrganizationEdit.js
+++ frontend/src/resources/Organization/OrganizationEdit.js
@@ -9,6 +9,16 @@ export const OrganizationEdit = props => (
         <SimpleForm redirect="show" >
             <TextInput source="pair:label" fullWidth />
             <MarkdownInput multiline source="pair:description" fullWidth />
+            <ReificationArrayInput source="pair:organizationOfRelationshipFrom" reificationClass="pair:RelationshipAssociation">
+              <ReferenceInput reference="Organization" source="pair:relationshipOrganizationTo">
+                <AutocompleteInput
+                  optionText={record => record && `${record['pair:label']}`}
+                />
+              </ReferenceInput>
+              <ReferenceInput label="Proximite" reference="RelationShipAssociationProximity" source="pair:relationshipAssociationProximityType">
+                <SelectInput optionText="pair:label" />
+              </ReferenceInput>
+            </ReificationArrayInput>
         </SimpleForm>
     </Edit>
 )
