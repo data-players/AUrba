edae0a81136f25038555db9b917d5b66aa2c11b4
simon <simon.louvet.zen@mail.com>
2021-11-04 00:28:44 +0100

    default datasource

--- frontend/src/resources/Organization/OrganizationCreate.js
+++ frontend/src/resources/Organization/OrganizationCreate.js
@@ -5,7 +5,7 @@ import { Create } from '@semapps/archipelago-layout';
 
 const OrganizationCreate = props => (
   <Create {...props}>
-    <SimpleForm initialValues={{"aurba:hasDataSource","https://64617461.AUrba.data-players.com/data-sources/a-urba"}}>
+    <SimpleForm initialValues={{"aurba:hasDataSource":"https://64617461.AUrba.data-players.com/data-sources/a-urba"}}>
       <TextInput source="pair:label" fullWidth />
       <ReferenceInput label="XXX" reference="DataSource" source="aurba:hasDataSource">
         <SelectInput optionText="pair:label"/>
