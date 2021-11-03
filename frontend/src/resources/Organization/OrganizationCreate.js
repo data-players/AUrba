b18f4cfc2b02499715df41557337c4af475da103
simon <simon.louvet.zen@mail.com>
2021-11-04 00:20:18 +0100

    default datasource

--- frontend/src/resources/Organization/OrganizationCreate.js
+++ frontend/src/resources/Organization/OrganizationCreate.js
@@ -7,7 +7,7 @@ const OrganizationCreate = props => (
   <Create {...props}>
     <SimpleForm>
       <TextInput source="pair:label" fullWidth />
-      <ReferenceInput reference="DataSource" source="aurba:hasDataSource">
+      <ReferenceInput label="XXX" reference="DataSource" source="aurba:hasDataSource">
         <SelectInput optionText="pair:label" initialValue="https://64617461.AUrba.data-players.com/data-sources/a-urba"/>
       </ReferenceInput>
     </SimpleForm>
